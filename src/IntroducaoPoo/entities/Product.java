17a68f6f56fb6f94d20d9c60163e965e127719e2
Georgio Azevedo <you@example.com>
2025-11-18 17:32:19 -0300

    feat: Adiciona sistema de estoque com classe Product e toString (aulas 67-69)

---
+++ src/IntroducaoPoo/entities/Product.java
@@ -0,0 +1,30 @@
+package IntroducaoPoo.entities;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProdutcs(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+            + ", $ "
+            + String.format("%.2f", price)
+            + ", "
+            + quantity
+            + " units, Total: $ "
+            + String.format("%.2f", totalValueInStock());
+    }
+}
