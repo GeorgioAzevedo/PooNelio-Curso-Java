ce452020a9d1e9141548c525bc0f764d056510b1
Georgio <georgio.filho123@gmail.com>
2026-07-14 22:37:24 -0300

    feat: Adiciona exemplos de construtores, this e sobrecarga

---
+++ src/ConstrutoresThisSobrecargaEncapsulamento/entities/Product.java
@@ -0,0 +1,47 @@
+package ConstrutoresThisSobrecargaEncapsulamento.entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price  = price;
+        this.quantity = quantity;
+    }
+
+    public Product (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProdutcs(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
